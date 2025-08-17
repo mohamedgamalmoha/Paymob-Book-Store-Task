043b806ae4fa393f7e87bcdf89b78cb35ff05307
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-18 00:16:59 +0300

    Add jwt views

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from accounts.api.views import UserViewSet
 
@@ -11,5 +12,7 @@ router = routers.DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls), name='accounts_routes'),
 ]
