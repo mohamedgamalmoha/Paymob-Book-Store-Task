805f61d975715d6734fd47130bd2d466c82de8a8
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-17 23:14:23 +0300

    Add accounts urls to core urls

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-from django.urls import path
+from django.urls import path, include
 
 from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView,
                                    SpectacularJSONAPIView, SpectacularYAMLAPIView)
@@ -26,6 +26,7 @@ from drf_spectacular.views import (SpectacularAPIView, SpectacularRedocView, Spe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('accounts.api.urls'), name='accounts'),
 ]
 
 
