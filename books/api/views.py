838488b1cb38dcbf399d12aebedc4348c0d19871
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-18 00:21:17 +0300

    Write views for books app

---
+++ books/api/views.py
@@ -0,0 +1,53 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+from accounts.api.permissions import IsAuthor, IsReviewer, IsOwner, ReadOnly
+from books.models import Book, Review, Favorites
+from books.api.serializers import BookSerializer, ReviewSerializer, FavoritesSerializer
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+        JWTAuthentication,
+    ]
+    permission_classes = [ReadOnly | IsAuthor]
+    serializer_class = BookSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class ReviewViewSet(ModelViewSet):
+    queryset = Review.objects.all()
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+        JWTAuthentication,
+    ]
+    permission_classes = [ReadOnly | IsReviewer]
+    serializer_class = ReviewSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(reviewer=self.request.user)
+
+
+class FavoritesViewSet(ModelViewSet):
+    queryset = Favorites.objects.all()
+    authentication_classes = [
+        BasicAuthentication,
+        SessionAuthentication,
+        JWTAuthentication,
+    ]
+    permission_classes = [IsOwner]
+    serializer_class = FavoritesSerializer
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
