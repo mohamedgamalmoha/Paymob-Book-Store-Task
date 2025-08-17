639ace84ca11767ed5c186dce3a2e181c7209db7
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-17 23:11:32 +0300

    Write user serializer

---
+++ accounts/api/serializers.py
@@ -0,0 +1,35 @@
+from django.core import exceptions as django_exceptions
+from django.contrib.auth.password_validation import validate_password
+
+from rest_framework import serializers
+
+from accounts.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        exclude = ('is_superuser', 'is_staff', 'groups', 'user_permissions')
+        read_only_fields = ('is_active', 'role', 'last_login', 'date_joined')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def validate(self, data):
+        user = User(**data)
+        password = data.get("password")
+
+        try:
+            validate_password(password, user)
+        except django_exceptions.ValidationError as e:
+            raise serializers.ValidationError(
+                {"password": e}
+            )
+
+        return data
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            ** validated_data
+        )
