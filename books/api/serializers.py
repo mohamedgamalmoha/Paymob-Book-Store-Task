9a569511693c50b630de6b7d4dd6d10e18efa467
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-18 20:41:24 +0300

    Add books models serializers expandable fields

--- books/api/serializers.py
+++ books/api/serializers.py
@@ -1,26 +1,39 @@
-from rest_framework import serializers
+from rest_flex_fields import FlexFieldsModelSerializer
+
 from books.models import Book, Review, Favorites
 
 
-class BookSerializer(serializers.ModelSerializer):
+class BookSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = Book
         exclude = ()
         read_only_fields = ('created_at', 'updated_at')
+        expandable_fields = {
+            'author': ('accounts.api.serializers.UserSerializer', {'many': False}),
+            'reviews': ('books.api.serializers.ReviewSerializer', {'many': True}),
+        }
 
 
-class ReviewSerializer(serializers.ModelSerializer):
+class ReviewSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = Review
         exclude = ()
         read_only_fields = ('created_at', 'updated_at')
+        expandable_fields = {
+            'book': ('books.api.serializers.BookSerializer', {'many': False}),
+            'reviewer': ('accounts.api.serializers.UserSerializer', {'many': False}),
+        }
 
 
-class FavoritesSerializer(serializers.ModelSerializer):
+class FavoritesSerializer(FlexFieldsModelSerializer):
 
     class Meta:
         model = Favorites
         exclude = ()
         read_only_fields = ('created_at', 'updated_at')
+        expandable_fields = {
+            'book': ('books.api.serializers.BookSerializer', {'many': False}),
+            'user': ('accounts.api.serializers.UserSerializer', {'many': False}),
+        }
