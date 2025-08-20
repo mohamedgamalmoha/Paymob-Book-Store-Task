e70beb9b7f5520f0856482799e940565daf8056e
mohamedgamalmoha <moha.gamal@nu.edu.eg>
2025-08-20 23:42:16 +0300

    Add refresh token

--- accounts/api/urls.py
+++ accounts/api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from accounts.api.views import UserViewSet
 
@@ -14,5 +14,6 @@ router.register(r"users", UserViewSet, basename="users")
 urlpatterns = [
     path("auth/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("auth/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("auth/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
     path("", include(router.urls), name="accounts_routes"),
 ]
